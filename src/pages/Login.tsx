9d40e71a0380b112c1976489a1a75d87bb794231
AndrewDrko <andrewmccartney09@gmail.com>
2024-09-17 14:44:21 -0600

    Adding Login Page Design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,12 +6,17 @@ import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import '../style/fonts.css';
+import { HiOutlineUserCircle } from 'react-icons/hi2';
+import { HiOutlineEye } from 'react-icons/hi2';
+import { HiOutlineEyeSlash } from 'react-icons/hi2';
 
 // import './styles.css';
 
 // import required modules
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const StyledSwiper = styled(Swiper)`
   position: relative;
@@ -24,56 +29,168 @@ const StyledSwiper = styled(Swiper)`
 `;
 
 const LoginContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
+  /* display: flex;
+  justify-content: space-between; */
   gap: 5rem;
-  font-size: 2.4rem;
+  font-size: 2rem;
+  /* width: 70rem; */
 
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  width: 60rem;
 
-  border-radius: 22px;
+  border-radius: 12px;
   padding: 5rem;
-  background-color: white;
+  box-shadow: var(--shadow-md);
+  /* background: linear-gradient(to right top, #572121, #82292a, #92292a, #a32929, #b32828); */
+  /* background-image: linear-gradient(
+    to right top,
+    #2e2424,
+    #4b2424,
+    #672222,
+    #811d1d,
+    #9b1313
+  ); */
+
+  background-color: var(--color-grey-300);
+
+  color: black;
 
   z-index: 99;
+
+  @media (max-width: 37.5em) {
+    width: 100%;
+    height: 100%;
+    border-radius: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 
-const SectionsContainer = styled.div`
+const SectionsContainerLeft = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.2rem;
+  gap: 1.4rem;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const SectionsContainerRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.4rem;
 `;
 
 const TextBox = styled.input`
   border: none;
-  border-bottom: 3px solid var(--color-red-800);
+
+  background-color: transparent;
+  color: black;
+  letter-spacing: 0.7px;
+  padding: 0.4rem;
+  width: 100%;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const TextFieldContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 3px solid var(--color-grey-400);
+  margin-bottom: 2rem;
+
+  & svg {
+    width: 3rem;
+    height: 3rem;
+    color: var(--color-grey-500);
+    transition: all 0.2s;
+  }
+`;
+
+const EyeContainer = styled.div`
+  cursor: pointer;
+
+  & svg:hover {
+    color: var(--color-grey-700);
+  }
 `;
 
 const Image = styled.img`
   width: 20rem;
+  filter: drop-shadow(0 2px 5px rgba(0, 0, 0, 0.459));
+`;
+
+const Labels = styled.label`
+  font-weight: bold;
+`;
+
+const ButtonSubmit = styled.button`
+  margin-top: 2rem;
+  background-color: var(--color-red-700);
+  color: var(--color-grey-200);
+  border: none;
+  padding: 0.7rem;
+
+  &:hover {
+    background-color: var(--color-red-800);
+  }
+`;
+
+const Slogan = styled.blockquote`
+  font-family: 'Brunshscn', sans-serif;
+  font-size: 2rem;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 2rem;
+  font-size: 5rem;
 `;
 
 export default function App() {
+  const [isClicked, setClicked] = useState(false);
   return (
     <>
       <LoginContainer>
-        <SectionsContainer>
+        <SectionsContainerLeft>
           <Image src="logo-dai.png" />
-          <h2>¡Bienvenido!</h2>
-          <blockquote>"El futuro, el hoy, el bien comienzo"</blockquote>
-          <blockquote>Albert Einstein</blockquote>
-        </SectionsContainer>
-        <SectionsContainer>
-          <h1>Iniciar Sesión</h1>
-          <label>Usuario</label>
-          <TextBox type="text" placeholder="Ingrese su Usuario"></TextBox>
-          <label>Contraseña</label>
-          <TextBox type="text" placeholder="Ingrese su Contraseña"></TextBox>
-          <button>Ingresar</button>
-        </SectionsContainer>
+          <Slogan>¡ Tu Soporte en el Camino !</Slogan>
+          <Title>¡Bienvenido!</Title>
+        </SectionsContainerLeft>
+        <SectionsContainerRight>
+          <h2>Iniciar Sesión</h2>
+          <Labels>Usuario</Labels>
+          <TextFieldContainer>
+            <TextBox
+              title="Completa este campo"
+              type="text"
+              placeholder="Ingrese su Usuario"
+              required
+            ></TextBox>
+            <HiOutlineUserCircle />
+          </TextFieldContainer>
+          <Labels>Contraseña</Labels>
+          <TextFieldContainer>
+            <TextBox
+              title="Completa este campo"
+              type={isClicked ? 'text' : 'password'}
+              placeholder="Ingrese su Contraseña"
+              required
+            ></TextBox>
+            <EyeContainer
+              title={isClicked ? 'Ocultar Contraseña' : 'Mostrar Contraseña'}
+              onClick={() => setClicked(!isClicked)}
+            >
+              {isClicked ? <HiOutlineEyeSlash /> : <HiOutlineEye />}
+            </EyeContainer>
+          </TextFieldContainer>
+          <ButtonSubmit>Ingresar</ButtonSubmit>
+        </SectionsContainerRight>
       </LoginContainer>
       <StyledSwiper
         spaceBetween={30}
@@ -100,6 +217,15 @@ export default function App() {
         </SwiperSlide>
         <SwiperSlide>
           <img src="./daiphotos/photo4.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./daiphotos/photo5.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./daiphotos/photo6.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./daiphotos/photo7.jpeg" />
         </SwiperSlide>
       </StyledSwiper>
     </>
