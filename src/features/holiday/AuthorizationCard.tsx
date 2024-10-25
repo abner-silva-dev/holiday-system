3c70f010cd0f98e10e5c9ef24ec17d1ed37cb256
AndrewDrko <andrewmccartney09@gmail.com>
2024-10-25 14:48:47 -0600

    Adding Time Tag Functions

--- src/features/holiday/AuthorizationCard.tsx
+++ src/features/holiday/AuthorizationCard.tsx
@@ -243,20 +243,36 @@ const DatesModal = styled.div`
   z-index: 10;
 `;
 
-const TimeTag = styled.label`
+interface PropsTimeTag {
+  $time: 'past' | 'present' | 'future';
+}
+
+const TimeTagStyled = styled.label<PropsTimeTag>`
   //COLORS
-  // Present:
-  /* color: #b91c1c;
-  border: 1px solid #ffd4d4;
-  background-color: #fee2e2; */
-  // Future:
-  /* color: #0369a1;
-  border: 1px solid #d1eeff;
-  background-color: #e0f2fe; */
-  // Past:
-  color: #15803d;
-  border: 1px solid #cbffde;
-  background-color: #dcfce7;
+  ${(props) => {
+    switch (props.$time) {
+      case 'past':
+        return `
+          color: #15803d;
+          border: 1px solid #cbffde;
+          background-color: #dcfce7;
+        `;
+      case 'present':
+        return `
+          color: #b91c1c;
+          border: 1px solid #ffd4d4;
+          background-color: #fee2e2;
+        `;
+      case 'future':
+        return `
+          color: #0369a1;
+          border: 1px solid #d1eeff;
+          background-color: #e0f2fe;
+        `;
+      default:
+        return null;
+    }
+  }}
 
   font-weight: 700;
   font-size: 1.4rem;
@@ -265,6 +281,25 @@ const TimeTag = styled.label`
   user-select: none;
   text-transform: uppercase;
 `;
+
+const TimeTag: React.FC<PropsTimeTag> = ({ $time }) => {
+  let periodName;
+
+  switch ($time) {
+    case 'past':
+      periodName = 'pasado';
+      break;
+    case 'present':
+      periodName = 'present';
+      break;
+    case 'future':
+      periodName = 'futuro';
+      break;
+    default:
+      periodName = 'present';
+  }
+  return <TimeTagStyled $time={$time}>{periodName}</TimeTagStyled>;
+};
 
 interface PropsAuthorizationCard {
   holiday: HolidayInfo;
@@ -336,7 +371,7 @@ const AuthorizationCard: React.FC<PropsAuthorizationCard> = ({ holiday }) => {
                 })}
               </CreatedAt>
             </Heading>
-            <TimeTag>pasado</TimeTag>
+            <TimeTag $time={holiday?.period} />
           </Row>
 
           <EmployedItem>
