5d5061de4575d03ed9d34dea5b46ff0efafd5b01
AndrewDrko <andrewmccartney09@gmail.com>
2024-11-01 17:20:23 -0600

    Preparation for Forms: FormPersonalReference and FormClinicInformation

--- src/features/users/request/FormClinicInformation.tsx
+++ src/features/users/request/FormClinicInformation.tsx
@@ -1,93 +1,120 @@
-import Button from "../../../ui/Button"
-import { Field, FieldCheck, FieldRadio, Form, FormContainer, Input, Label, Page, PageChange, Percentage, Title } from "../../../ui/FormPieces"
+import { useForm } from 'react-hook-form';
+import Button from "../../../ui/Button";
+import {
+  Field,
+  FieldCheck,
+  FieldRadio,
+  Form,
+  FormContainer,
+  Input,
+  Label,
+  Page,
+  PageChange,
+  Percentage,
+  Title,
+} from "../../../ui/FormPieces";
 
-const FormClinicInformation = ({ handleBack }: { handleBack: () => void }) => {
-    return(
-        <Form>
-        <Page>
-            <Title as="h2">INFORMACIÓN CLÍNICA</Title>
-            <FormContainer>
-              <Field>
-                <Label>Estatura</Label>
-                <Percentage>
-                  <Input type="number"></Input>
-                  <span>cm</span>
-                </Percentage>
-              </Field>
-              <Field>
-                <Label>Peso</Label>
-                <Percentage>
-                  <Input type="number"></Input>
-                  <span>Kg</span>
-                </Percentage>
-              </Field>
-              <Field>
-                <Label>Tipo de Sangre</Label>
-                <Input type="text"></Input>
-              </Field>
-              <Field>
-                <Label>
-                  ¿Padece o padeció enfermedades que necesiten atención médica?
-                </Label>
-                <FieldRadio>
-                  <span>Sí</span>
-                  <Input type="radio" name="car"></Input>
-                  <span>No</span>
-                  <Input type="radio" name="car"></Input>
-                </FieldRadio>
-              </Field>
-              <Field>
-                <Label>¿Qué enfermedades?</Label>
-                <Input type="text"></Input>
-              </Field>
-              <Field>
-                <Label>
-                  ¿En el transcurso del último año ha tenido algún tipo de intervención
-                  quirúrgica?
-                </Label>
-                <FieldRadio>
-                  <span>Sí</span>
-                  <Input type="radio" name="car"></Input>
-                  <span>No</span>
-                  <Input type="radio" name="car"></Input>
-                </FieldRadio>
-              </Field>
-              <Field>
-                <Label>¿De qué tipo?</Label>
-                <Input type="text"></Input>
-              </Field>
-              <Field>
-                <Label>
-                  ¿Tiene algún inpedimento físico que lo limite en su trabajo?
-                </Label>
-                <FieldRadio>
-                  <span>Sí</span>
-                  <Input type="radio" name="car"></Input>
-                  <span>No</span>
-                  <Input type="radio" name="car"></Input>
-                </FieldRadio>
-              </Field>
-            </FormContainer>
-            <FieldCheck>
-              <Label>
-                Certifico que la información aquí proporcionada es verídica, por lo que
-                autorizo a Distribuidora de Auto Industrias, S.A de C.V, a corroborarla
-                cuestionando a las referencias señaladas tanto sobre mi conducta personal
-                como laboral, así como el uso de mis datos personales con base en el aviso
-                de privacidad de la empresa, el cual podré consultar en www.dai.com.mx,
-                reconociemdo que la presente solicitud no significa el establecimiento de
-                ninguna relación laboral, ni crea el compromiso de que ésta me otorgue.
-              </Label>
-              <Input type="checkbox"></Input>
-            </FieldCheck>
-            <PageChange>
-              <Button $variation="primary" onClick={handleBack}>
-                Atrás
-              </Button>
-            </PageChange>
-          </Page>
-          </Form>
-    );
+interface IFormClinicInformation {
+  height: number;
+  weight: number;
+  bloodType: string;
+  hasMedicalConditions: string;
+  medicalConditions: string;
+  hasSurgery: string;
+  surgeryType: string;
+  hasPhysicalImpediment: string;
+  certifyInformation: boolean;
 }
 
-export default FormClinicInformation
+const FormClinicInformation = ({ handleBack }: { handleBack: () => void }) => {
+  const { register, handleSubmit } = useForm<IFormClinicInformation>();
+
+  const onSubmit = (data: IFormClinicInformation) => {
+    console.log(data);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Page>
+        <Title as="h2">INFORMACIÓN CLÍNICA</Title>
+        <FormContainer>
+          <Field>
+            <Label htmlFor="height">Estatura</Label>
+            <Percentage>
+              <Input id="height" type="number" {...register('height', { required: true })} />
+              <span>cm</span>
+            </Percentage>
+          </Field>
+          <Field>
+            <Label htmlFor="weight">Peso</Label>
+            <Percentage>
+              <Input id="weight" type="number" {...register('weight', { required: true })} />
+              <span>Kg</span>
+            </Percentage>
+          </Field>
+          <Field>
+            <Label htmlFor="bloodType">Tipo de Sangre</Label>
+            <Input id="bloodType" type="text" {...register('bloodType', { required: true })} />
+          </Field>
+          <Field>
+            <Label>¿Padece o padeció enfermedades que necesiten atención médica?</Label>
+            <FieldRadio>
+              <span>Sí</span>
+              <Input id="hasMedicalConditionsYes" type="radio" value="Sí" {...register('hasMedicalConditions')} />
+              <span>No</span>
+              <Input id="hasMedicalConditionsNo" type="radio" value="No" {...register('hasMedicalConditions')} />
+            </FieldRadio>
+          </Field>
+          <Field>
+            <Label htmlFor="medicalConditions">¿Qué enfermedades?</Label>
+            <Input id="medicalConditions" type="text" {...register('medicalConditions')} />
+          </Field>
+          <Field>
+            <Label>¿En el transcurso del último año ha tenido algún tipo de intervención quirúrgica?</Label>
+            <FieldRadio>
+              <span>Sí</span>
+              <Input id="hasSurgeryYes" type="radio" value="Sí" {...register('hasSurgery')} />
+              <span>No</span>
+              <Input id="hasSurgeryNo" type="radio" value="No" {...register('hasSurgery')} />
+            </FieldRadio>
+          </Field>
+          <Field>
+            <Label htmlFor="surgeryType">¿De qué tipo?</Label>
+            <Input id="surgeryType" type="text" {...register('surgeryType')} />
+          </Field>
+          <Field>
+            <Label>¿Tiene algún impedimento físico que lo limite en su trabajo?</Label>
+            <FieldRadio>
+              <span>Sí</span>
+              <Input id="hasPhysicalImpedimentYes" type="radio" value="Sí" {...register('hasPhysicalImpediment')} />
+              <span>No</span>
+              <Input id="hasPhysicalImpedimentNo" type="radio" value="No" {...register('hasPhysicalImpediment')} />
+            </FieldRadio>
+          </Field>
+        </FormContainer>
+        <FieldCheck>
+          <Label>
+            Certifico que la información aquí proporcionada es verídica, por lo que
+            autorizo a Distribuidora de Auto Industrias, S.A de C.V, a corroborarla
+            cuestionando a las referencias señaladas tanto sobre mi conducta personal
+            como laboral, así como el uso de mis datos personales con base en el aviso
+            de privacidad de la empresa, el cual podré consultar en www.dai.com.mx,
+            reconociendo que la presente solicitud no significa el establecimiento de
+            ninguna relación laboral, ni crea el compromiso de que ésta me otorgue.
+          </Label>
+          <Input id="certifyInformation" type="checkbox" {...register('certifyInformation', { required: true })} />
+        </FieldCheck>
+        <PageChange>
+          <Button $variation="primary" type="submit">
+            Enviar
+          </Button>
+          <Button $variation="primary" onClick={handleBack}>
+            Atrás
+          </Button>
+        </PageChange>
+      </Page>
+    </Form>
+  );
+}
+
+export default FormClinicInformation;
--- src/features/users/request/FormPersonalReference.tsx
+++ src/features/users/request/FormPersonalReference.tsx
@@ -1,86 +1,127 @@
+import { useForm } from 'react-hook-form';
 import Button from "../../../ui/Button";
-import { Field, Form, FormContainer, Input, Label, Page, PageChange, Title } from "../../../ui/FormPieces";
+import {
+  Field,
+  Form,
+  FormContainer,
+  Input,
+  Label,
+  Page,
+  PageChange,
+  Title,
+} from "../../../ui/FormPieces";
 
-const FormPersonalReference = ({ handleNext, handleBack }: {handleNext:()=> void, handleBack:() => void }) => {
-return(
-    <Form>
-    <Page>
-    <Title as="h2">REFERENCIAS PERSONALES</Title>
-    <Title as="h4">
-      Favor de incluir los datos de 3 personas que lo conozcan bien, que no sean
-      familiares
-    </Title>
+interface IFormPersonalReference {
+  person1: {
+    name: string;
+    duration: string;
+    phone: string;
+    occupation: string;
+  };
+  person2: {
+    name: string;
+    duration: string;
+    phone: string;
+    occupation: string;
+  };
+  person3: {
+    name: string;
+    duration: string;
+    phone: string;
+    occupation: string;
+  };
+}
 
-    <Title as="h3">Persona 1</Title>
-    <FormContainer>
-      <Field>
-        <Label>Nombre Completo</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Tiempo de Conocerle</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Teléfono / Celular</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Ocupación</Label>
-        <Input type="text"></Input>
-      </Field>
-    </FormContainer>
+const FormPersonalReference = ({ handleNext, handleBack }: { handleNext: () => void; handleBack: () => void; }) => {
+  const { register, handleSubmit } = useForm<IFormPersonalReference>();
 
-    <Title as="h3">Persona 2</Title>
-    <FormContainer>
-      <Field>
-        <Label>Nombre Completo</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Tiempo de Conocerle</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Teléfono / Celular</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Ocupación</Label>
-        <Input type="text"></Input>
-      </Field>
-    </FormContainer>
+  const onSubmit = (data: IFormPersonalReference) => {
+    console.log(data);
+    // Aquí puedes agregar la lógica para guardar los datos
+  };
 
-    <Title as="h3">Persona 3</Title>
-    <FormContainer>
-      <Field>
-        <Label>Nombre Completo</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Tiempo de Conocerle</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Teléfono / Celular</Label>
-        <Input type="text"></Input>
-      </Field>
-      <Field>
-        <Label>Ocupación</Label>
-        <Input type="text"></Input>
-      </Field>
-    </FormContainer>
-    <PageChange>
-      <Button $variation="primary" onClick={handleBack}>
-        Atrás
-      </Button>
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Page>
+        <Title as="h2">REFERENCIAS PERSONALES</Title>
+        <Title as="h4">
+          Favor de incluir los datos de 3 personas que lo conozcan bien, que no sean familiares
+        </Title>
 
-      <Button $variation="confirm" onClick={handleNext}>
-        Siguiente
-      </Button>
-    </PageChange>
-  </Page>
-  </Form>
-);
-} 
+        <Title as="h3">Persona 1</Title>
+        <FormContainer>
+          <Field>
+            <Label htmlFor="person1Name">Nombre Completo</Label>
+            <Input id="person1.name" type="text" {...register('person1.name', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person1Duration">Tiempo de Conocerle</Label>
+            <Input id="person1.duration" type="text" {...register('person1.duration', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person1Phone">Teléfono / Celular</Label>
+            <Input id="person1.phone" type="text" {...register('person1.phone', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person1Occupation">Ocupación</Label>
+            <Input id="person1.occupation" type="text" {...register('person1.occupation', { required: true })} />
+          </Field>
+        </FormContainer>
+
+        <Title as="h3">Persona 2</Title>
+        <FormContainer>
+          <Field>
+            <Label htmlFor="person2Name">Nombre Completo</Label>
+            <Input id="person2.name" type="text" {...register('person2.name', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person2Duration">Tiempo de Conocerle</Label>
+            <Input id="person2.duration" type="text" {...register('person2.duration', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person2Phone">Teléfono / Celular</Label>
+            <Input id="person2.phone" type="text" {...register('person2.phone', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person2Occupation">Ocupación</Label>
+            <Input id="person2.occupationon" type="text" {...register('person2.occupation', { required: true })} />
+          </Field>
+        </FormContainer>
+
+        <Title as="h3">Persona 3</Title>
+        <FormContainer>
+          <Field>
+            <Label htmlFor="person3Name">Nombre Completo</Label>
+            <Input id="person3.name" type="text" {...register('person3.name', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person3Duration">Tiempo de Conocerle</Label>
+            <Input id="person3.duration" type="text" {...register('person3.duration', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person3Phone">Teléfono / Celular</Label>
+            <Input id="person3.phone" type="text" {...register('person3.phone', { required: true })} />
+          </Field>
+          <Field>
+            <Label htmlFor="person3Occupation">Ocupación</Label>
+            <Input id="person3.occupation" type="text" {...register('person3.occupation', { required: true })} />
+          </Field>
+        </FormContainer>
+
+        <PageChange>
+          <Button $variation="primary" onClick={handleBack}>
+            Atrás
+          </Button>
+          <Button $variation="confirm" type="submit">
+            Guardar
+          </Button>
+          <Button $variation="confirm" onClick={handleNext}>
+            Siguiente
+          </Button>
+        </PageChange>
+      </Page>
+    </Form>
+  );
+}
+
 export default FormPersonalReference;
