1c4e37d085eb4740ffa1cb08e946bb4ec9713b06
AndrewDrko <andrewmccartney09@gmail.com>
2024-11-07 13:27:41 -0600

    updated address field

--- src/features/users/request/FormPersonalData.tsx
+++ src/features/users/request/FormPersonalData.tsx
@@ -17,6 +17,7 @@ import { useUser2 } from '../useUser';
 import { useUpdateRequest } from './useUpdateRequest';
 import { updateUser } from '../../../services/apiUsers';
 import { useUpdateUser } from '../useUpdateUser';
+import styled from 'styled-components';
 
 interface IFormPersonalData {
   name: string;
@@ -31,6 +32,10 @@ interface IFormPersonalData {
   homePhone: string;
   mobilePhone: string;
 }
+
+const FieldAdd = styled(Field)`
+grid-column: 2 / 4;
+`;
 
 function FormPersonalData({ handleNext }: { handleNext: () => void }) {
   const { user, isPending } = useUser2();
@@ -111,14 +116,14 @@ function FormPersonalData({ handleNext }: { handleNext: () => void }) {
             </Select>
           </Field>
 
-          <Field>
+          <FieldAdd>
             <Label>Domicilio: Calle, Número, Colonia y Delegación</Label>
             <Input
               type="text"
               id="address"
               {...register('address', { required: true })}
             />
-          </Field>
+          </FieldAdd>
 
           <Field>
             <Label>Código Postal</Label>
